e4dd9e206654b5614ffe7d209b9cb81eabd8b520
Nikesh <nikeshniki19@gmail.com>
2025-02-16 17:38:03 -0500

    frontend props states events

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./Card.css"
 
-type Props = {}
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
 
-const Card = (props: Props) => {
+const Card: React.FC<Props> = ({companyName, ticker, price}: Props) : JSX.Element=> {
   return (
     <div className='card'>
         <img src="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg"></img>
         <div className="details">
-            <h2>Apple</h2>
-            <p>$110</p>
+            <h2>{companyName} ({ticker})</h2>
+            <p>${price}</p>
         </div>
         <p className='info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo fugit atque facere ut vitae praesentium aspernatur minima cupiditate quia natus exercitationem blanditiis quod sint accusantium, quis corporis fuga aperiam?</p>
     </div>
