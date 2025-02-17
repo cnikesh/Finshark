8c5def069c59b08fe8e7a0f974e212eef6caf342
Nikesh <nikeshniki19@gmail.com>
2025-02-16 20:18:48 -0500

    conditional rendering

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,18 +1,12 @@
 import React, { ChangeEvent, useState, SyntheticEvent } from 'react'
 
-type Props = {}
+interface Props {
+    onClick: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-const Search : React.FC = (props: Props) : JSX.Element => {
-    const [search, setSearch] = useState<string>();
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    }
-
-    const onClick = (event: SyntheticEvent) => {
-        console.log(event);
-    };
+const Search : React.FC = ({onClick, search, handleChange}: Props) : JSX.Element => {
 
   return (
     <div>
