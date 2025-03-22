b000120a1a3f7635679cb55a34f6f70d20a29931
Nikesh <nikeshniki19@gmail.com>
2025-03-22 01:18:16 -0400

    la

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -15,7 +15,7 @@ import ProtectedRoute from "./ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/Finshark/",
     element: <App />,
     children: [
       { path: "", element: <HomePage /> },
